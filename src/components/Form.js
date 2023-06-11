386803baac0e10bd1e346554813b220d84c5001b
Garen <karmartir@gmail.com>
2023-06-11 11:32:34 -0400

    2nd commit

---
+++ src/components/Form.js
@@ -0,0 +1,58 @@
+import React, {useEffect} from 'react';
+import {nanoid} from "nanoid";
+
+const Form = ({input, setInput, todos, setTodos, editTodo, setEditTodo}) => {
+
+    const updateTodo = ( id, title, completed) => {
+        const newTodo = todos.map(todo =>
+        todo.id === id ? { id, title, completed} : todo
+        )
+        setTodos(newTodo)
+        setEditTodo('')
+    }
+
+    useEffect(() => {
+        if(editTodo) {
+            setInput(editTodo.title);
+        } else {
+            setInput('')
+        }
+    }, [setInput, editTodo])
+
+    const onInputChange = (e) => {
+        setInput(e.target.value)
+    }
+
+    const onFormSubmit = (e) => {
+        e.preventDefault()
+
+        if (!editTodo) {
+            setTodos([...todos, {id: nanoid(), title: input, completed: false}])
+            setInput('')
+        } else {
+            updateTodo( editTodo.id,input, editTodo.completed)
+        }
+    }
+
+    return (
+        <form onSubmit={onFormSubmit}>
+            <input
+                type="text"
+                placeholder='Enter Task'
+                className='task-input'
+                value={input}
+                required
+                onChange={onInputChange}
+            />
+
+            <button
+                className='button-add'
+                type='submit'
+            >
+                {editTodo ? 'Change' : 'Add'}
+            </button>
+        </form>
+    );
+};
+
+export default Form;
---
+++ src/components/TodoList.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const TodoList = ({todos, setTodos, setEditTodo}) => {
+
+    const handleComplete = (todo) => {
+        setTodos(
+            todos.map(item => {
+                if (item.id === todo.id) {
+                    return {...item, completed: !item.completed}
+                }
+                return item
+            })
+        )
+    }
+    const handleEdit = ({id}) => {
+        const findTodo = todos.find(todo => todo.id === id)
+        setEditTodo(findTodo)
+    }
+
+
+    const handleDelete = ({id}) => {
+        setTodos(todos.filter(todo => todo.id !== id))
+    }
+
+    return (
+        <div>
+            {todos.map(todo =>
+                <li className='List-item'
+                    key={todo.id}
+                >
+                    <input
+                        type='text'
+                        value={todo.title}
+                        className={`list ${todo.completed ? "complete" : ""}`}
+                        onChange={event => event.preventDefault()}
+                    />
+
+                    <div>
+                        <button onClick={() => handleComplete(todo)}
+                                className='button complete task button'> Complete </button>
+
+                        <button onClick={() => handleEdit(todo)}
+                                className='button edit task button'> Edit</button>
+
+                        <button onClick={() => handleDelete(todo)}
+                                className='button delete task button'> Delete </button>
+                    </div>
+
+                </li>
+            )}
+        </div>
+    );
+};
+
+export default TodoList;
